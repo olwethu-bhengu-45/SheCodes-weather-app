a38cc66a77aaa5e030694552c20a24c22851caff
Olwethu Bhengu <olwethubhengu45@gmail.com>
2025-07-10 23:37:52 +0200

    Injected HTML forecast in JS

--- src/index.js
+++ src/index.js
@@ -51,7 +51,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function showForecast() {
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>25°</strong>
+              </div>
+              <div class="weather-forecast-temperature">14°</div>
+            </div>
+        </div> `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Durban");
+showForecast();
